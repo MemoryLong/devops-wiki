adbb599d973d423237960386e9ec03428ddf2939
MemoryLong <haifeng222222@sina.com>
2023-02-11 17:10:29 +0800

    jenkins流水线调试

--- apitest/createReport.sh
+++ apitest/createReport.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd /appdata/devops-wiki/apitest
-mkdir report
 #D:
 #cd D:\Dream Future Project\workspace\devops-wiki\apitest
 ## -e 环境变量; -g 全局变量; -k 禁止SSL; -n 循环次数; --bail 失败后不再往下执行; -r 报告格式
