2ea28cafe74fc67212bca1c7ae9565015a2e2890
MemoryLong <haifeng222222@sina.com>
2023-02-11 16:53:48 +0800

    生成报告脚本修改为linux脚本

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # devops-wiki
-接口测试：postman+newman+git+jenkins
+**接口测试**：postman+newman+git+jenkins
+
+**目标**：生成接口测试web报告
+
+**步骤**：
+1. postman手工导出collections
+2. postman手工导出环境变量、全局变量
+3. 执行web报告生成脚本createReport.bat
--- apitest/createReport.bat
+++ apitest/createReport.bat
@@ -1,5 +1,7 @@
 #!/bin/bash
-D:
-cd D:\Dream Future Project\workspace\devops-wiki\apitest
+cd /appdata/devops-wiki/apitest
+mkdir report
+#D:
+#cd D:\Dream Future Project\workspace\devops-wiki\apitest
 ## -e 环境变量; -g 全局变量; -k 禁止SSL; -n 循环次数; --bail 失败后不再往下执行; -r 报告格式
 newman run dev_user.postman_collection.json -e localhost.postman_environment.json -g MyWorkspace.postman_globals.json -k -n 2 --bail -r htmlextra,html,json,junit,cli
