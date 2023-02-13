e86ef12eff04c804134e9dc54a6f0c0447bd7bba
MemoryLong <haifeng222222@sina.com>
2023-02-13 20:40:24 +0800

    jenkins HTML报告示例

--- apitest/createReport.sh
+++ apitest/createReport.sh
@@ -3,4 +3,4 @@ cd /appdata/devops-wiki/apitest
 #D:
 #cd D:\Dream Future Project\workspace\devops-wiki\apitest
 ## -e 环境变量; -g 全局变量; -k 禁止SSL; -n 循环次数; --bail 失败后不再往下执行; -r 报告格式
-newman run dev_user.postman_collection.json -e localhost.postman_environment.json -g MyWorkspace.postman_globals.json -k -n 2 --bail -r html,htmlextra,json,cli,junit
+newman run dev_user.postman_collection.json -e localhost.postman_environment.json -g MyWorkspace.postman_globals.json -k -n 2 --bail -r html,htmlextra,json,cli,junit --reporter-html-export newmanhtmlextra.html
